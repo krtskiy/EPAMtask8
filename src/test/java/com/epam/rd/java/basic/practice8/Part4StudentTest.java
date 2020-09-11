d541ed44fac29f9052f9a5418774758c0c8cd90c
jMushmyH <jMushmyH@email.com>
2020-09-11 23:49:11 +0300

    test

--- src/test/java/com/epam/rd/java/basic/practice8/Part4StudentTest.java
+++ src/test/java/com/epam/rd/java/basic/practice8/Part4StudentTest.java
@@ -1,14 +1,5 @@
 package com.epam.rd.java.basic.practice8;
 
-import org.junit.Assert;
-import org.junit.Test;
-
 public class Part4StudentTest {
-
-    @Test
-    public void testPlaceholder() {
-        Demo.main(null);
-        Assert.assertTrue(true);
-    }
 
 }
