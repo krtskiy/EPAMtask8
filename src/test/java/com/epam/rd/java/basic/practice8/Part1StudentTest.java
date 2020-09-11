8392b896ba72a59e1d3ab30d91f5abd458de99f2
jMushmyH <jMushmyH@email.com>
2020-09-11 05:09:09 +0300

    test placeholders deleted

--- src/test/java/com/epam/rd/java/basic/practice8/Part1StudentTest.java
+++ src/test/java/com/epam/rd/java/basic/practice8/Part1StudentTest.java
@@ -17,43 +17,4 @@ import java.util.Properties;
 
 public class Part1StudentTest {
 
-    private static Connection connection;
-    private static DBManager dbManager;
-
-    private static final String PROPERTIES = "app.properties";
-    private static final String CONNECTION_URL = "connection.url";
-
-    @BeforeClass
-    public static void getConnection() throws IOException, SQLException {
-        try (InputStream is = new FileInputStream(PROPERTIES)) {
-            Properties prop = new Properties();
-            prop.load(is);
-            connection = DriverManager.getConnection(prop.getProperty(CONNECTION_URL));
-
-            dbManager = DBManager.getInstance();
-        }
-    }
-
-    // TODO
-    @Test
-    public void testsPlaceholder() {
-        try {
-            Demo.main(null);
-            DBManager dbManager = DBManager.getInstance();
-            dbManager.insertUser(User.createUser("ivan"));
-            dbManager.insertTeam(Team.createTeam("teamIvan"));
-            dbManager.findAllUsers();
-            dbManager.findAllTeams();
-            dbManager.getTeam("teamIvan");
-            dbManager.getUser("ivan");
-            dbManager.deleteTeam(null);
-            dbManager.updateTeam(null);
-            DBManager.main(null);
-        } catch (Exception e) {
-            e.getCause();
-            Assert.assertTrue(true);
-        }
-    }
-
-
 }
