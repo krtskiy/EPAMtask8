88c8b337e9e10f1cc3c2cceb3c0df92770aebfb3
jMushmyH <jMushmyH@email.com>
2020-09-12 00:47:02 +0300

    test

--- src/test/java/com/epam/rd/java/basic/practice8/Part1StudentTest.java
+++ src/test/java/com/epam/rd/java/basic/practice8/Part1StudentTest.java
@@ -1,5 +1,26 @@
 package com.epam.rd.java.basic.practice8;
 
+import com.epam.rd.java.basic.practice8.db.DBManager;
+import com.epam.rd.java.basic.practice8.db.entity.User;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
 public class Part1StudentTest {
 
+    private static DBManager dbManager;
+
+    @BeforeClass
+    public static void preparingForTask1Tests() {
+        dbManager = DBManager.getInstance();
+    }
+
+    @Test
+    public void shouldInsertNewUser() {
+        int usersLengthBefore = dbManager.findAllUsers().size();
+        dbManager.insertUser(User.createUser("testUser"));
+        int usersLengthAfter = dbManager.findAllUsers().size();
+
+        Assert.assertEquals(usersLengthAfter, usersLengthBefore + 1);
+    }
 }
