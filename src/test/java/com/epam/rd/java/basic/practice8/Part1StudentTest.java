4063824a8a7b052f06422aa00015d6018367a03d
jMushmyH <jMushmyH@email.com>
2020-09-11 04:18:47 +0300

    tests

--- src/test/java/com/epam/rd/java/basic/practice8/Part1StudentTest.java
+++ src/test/java/com/epam/rd/java/basic/practice8/Part1StudentTest.java
@@ -1,6 +1,8 @@
 package com.epam.rd.java.basic.practice8;
 
 import com.epam.rd.java.basic.practice8.db.DBManager;
+import com.epam.rd.java.basic.practice8.db.entity.Team;
+import com.epam.rd.java.basic.practice8.db.entity.User;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -15,40 +17,35 @@ import java.util.Properties;
 
 public class Part1StudentTest {
 
-//    private static Connection connection;
-//    private static DBManager dbManager;
-//
-//    private static final String PROPERTIES = "app.properties";
-//    private static final String CONNECTION_URL = "connection.url";
-//
-//    @BeforeClass
-//    public static void getConnection() throws IOException, SQLException {
-//        try (InputStream is = new FileInputStream(PROPERTIES)) {
-//            Properties prop = new Properties();
-//            prop.load(is);
-//            connection = DriverManager.getConnection(prop.getProperty(CONNECTION_URL));
-//
-//            dbManager = DBManager.getInstance();
-//        }
-//    }
+    private static Connection connection;
+    private static DBManager dbManager;
 
-    @Test
-    public void demoMainTestPlaceholder() {
-        Demo.main(null);
-        Assert.assertTrue(true);
+    private static final String PROPERTIES = "app.properties";
+    private static final String CONNECTION_URL = "connection.url";
+
+    @BeforeClass
+    public static void getConnection() throws IOException, SQLException {
+        try (InputStream is = new FileInputStream(PROPERTIES)) {
+            Properties prop = new Properties();
+            prop.load(is);
+            connection = DriverManager.getConnection(prop.getProperty(CONNECTION_URL));
+
+            dbManager = DBManager.getInstance();
+        }
     }
 
+    // TODO
     @Test
-    public void dbManagerTestPlaceholder() {
+    public void testsPlaceholder() {
         try {
+            Demo.main(null);
             DBManager dbManager = DBManager.getInstance();
-            dbManager.insertUser(null);
-            dbManager.insertTeam(null);
-            dbManager.getConnection(null);
+            dbManager.insertUser(User.createUser("ivan"));
+            dbManager.insertTeam(Team.createTeam("teamIvan"));
             dbManager.findAllUsers();
             dbManager.findAllTeams();
-            dbManager.getTeam(null);
-            dbManager.getUser(null);
+            dbManager.getTeam("teamIvan");
+            dbManager.getUser("ivan");
             dbManager.deleteTeam(null);
             dbManager.updateTeam(null);
             DBManager.main(null);
