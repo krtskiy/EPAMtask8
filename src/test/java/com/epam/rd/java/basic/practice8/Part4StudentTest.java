41df733ad6c62df68fdcca5dfe36deca06f5c014
jMushmyH <jMushmyH@email.com>
2020-09-11 05:54:50 +0300

    test placeholders

--- src/test/java/com/epam/rd/java/basic/practice8/Part4StudentTest.java
+++ src/test/java/com/epam/rd/java/basic/practice8/Part4StudentTest.java
@@ -1,5 +1,14 @@
 package com.epam.rd.java.basic.practice8;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class Part4StudentTest {
+
+    @Test
+    public void testPlaceholder() {
+        Demo.main(null);
+        Assert.assertTrue(true);
+    }
 
 }
